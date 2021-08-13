0864a7499eb26657dae0519541eef10e1096a9d0
maickol12 <rocm960107m@gmail.com>
2021-08-13 00:13:18 -0500

    docs

--- src/helper/getGifs.js
+++ src/helper/getGifs.js
@@ -2,7 +2,7 @@
  
  export const getGifs = async( category ) => {
     
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&api_key=emDLS5x82wPczycH9HcUNRWDNrJLJVTd&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&api_key=emDLS5x82wPczycH9HcUNRWDNrJLJVTd&limit=10`;
      const resp      = await fetch( url );
      const { data }  = await resp.json();
      
